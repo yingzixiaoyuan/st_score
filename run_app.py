2ba646bb4155d4d3e9b3f35df46d17aaa04933f5
chulihui <chulihui@nibirutech.com>
2025-08-24 15:58:26 +0800

    test: t1

--- run_app.py
+++ run_app.py
@@ -2,6 +2,12 @@ import os
 import sys
 from pathlib import Path
 
+os.environ["STREAMLIT_DEVELOPMENT_MODE"] = "0"
+os.environ["STREAMLIT_ENV"] = "production"
+os.environ["BROWSER"] = "none"
+os.environ["STREAMLIT_SERVER_ENABLE_CORS"] = "false"
+os.environ["STREAMLIT_SERVER_ENABLE_XSRF_PROTECTION"] = "false"
+os.environ["STREAMLIT_BROWSER_GATHER_USAGE_STATS"] = "false"
 import streamlit.web.cli as stcli
 
 
@@ -9,6 +15,20 @@ def resolve_path(path: str) -> str:
     base_path = getattr(sys, "_MEIPASS", os.getcwd())
     return str(Path(base_path) / path)
 
+
+config_dir = os.path.join(os.path.expanduser("~"), ".streamlit")
+os.makedirs(config_dir, exist_ok=True)
+with open(os.path.join(config_dir, "config.toml"), "w", encoding="utf-8") as f:
+    f.write("""
+[server]
+headless = true
+port = 8501
+enableCORS = false
+enableXsrfProtection = false
+
+[browser]
+gatherUsageStats = false
+""")
 
 if __name__ == "__main__":
     sys.argv = [
